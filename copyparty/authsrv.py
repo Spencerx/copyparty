23c2a58b68144ed5a482d494e22e6be02c267ead
ed <s@ocv.me>
2026-01-09 00:17:04 +0000

    append: only allow with reflink or !dedup;
    un-dedup of the target file is likely too expensive, and
    relying on up2k.db to do it conditionally too dangerous

--- copyparty/authsrv.py
+++ copyparty/authsrv.py
@@ -2650,6 +2650,9 @@ class AuthSrv(object):
                 self.log(t, 1)
                 raise Exception(t)
 
+            if "dedup" in vol.flags and "reflink" not in vol.flags:
+                vol.flags["apnd_who"] = "ndd"
+
             # verify tags mentioned by -mt[mp] are used by -mte
             local_mtp = {}
             local_only_mtp = {}
